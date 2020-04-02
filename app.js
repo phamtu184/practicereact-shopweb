8c748719c8559c8f266575b72ec2d43edafbee5b
tu pham <shammer280@gmail.com>
2020-04-02 18:07:00 +0700

    first heroku push

--- app.js
+++ app.js
@@ -13,17 +13,20 @@ var productRouter = require('./src/routes/product.route');
 var app = express();
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify:false});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("Mongoose success connect")
 })
 
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use('/user', usersRouter);
 app.use('/auth', authRouter);
---
+++ client/build/precache-manifest.0cc4ad15e1d4be76be2d0103935d8442.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5259345015ddc68cea27ce8a448b8640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5806e86aac345d146eca",
+    "url": "/static/css/2.267a744b.chunk.css"
+  },
+  {
+    "revision": "4d4c8095306381b41f89",
+    "url": "/static/css/main.57a068e5.chunk.css"
+  },
+  {
+    "revision": "5806e86aac345d146eca",
+    "url": "/static/js/2.eda901e8.chunk.js"
+  },
+  {
+    "revision": "1b6842fa839c6355cc57dc5d66fa4496",
+    "url": "/static/js/2.eda901e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d4c8095306381b41f89",
+    "url": "/static/js/main.936ecabf.chunk.js"
+  },
+  {
+    "revision": "62caab877184bd7db7f5",
+    "url": "/static/js/runtime-main.e513ea11.js"
+  },
+  {
+    "revision": "0586ad606fa35f73282ce7ffc3267ec0",
+    "url": "/static/media/back.0586ad60.svg"
+  },
+  {
+    "revision": "b0813e15d8c6e907d47c17bc79793dde",
+    "url": "/static/media/background.b0813e15.jpg"
+  },
+  {
+    "revision": "384981054dcd90c68ceddb3f7a49d9e1",
+    "url": "/static/media/background2.38498105.jpg"
+  },
+  {
+    "revision": "484b3237ff352e5847ad54d33d223cff",
+    "url": "/static/media/background3.484b3237.jpg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/bin.650cdc57.svg"
+  },
+  {
+    "revision": "051b5a0feb755f40311ddf30df185bb1",
+    "url": "/static/media/girl.051b5a0f.jpg"
+  },
+  {
+    "revision": "55f6849429de279d473c459c7ead3249",
+    "url": "/static/media/menu.55f68494.svg"
+  },
+  {
+    "revision": "2586956f0572cac92a20684f022e4643",
+    "url": "/static/media/pug.2586956f.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4e4cb6a802affe0fe4b42899fd82f3d8",
+    "url": "/static/media/supermarket.4e4cb6a8.svg"
+  },
+  {
+    "revision": "7de5e5f7d3fbd734c8d9328cc6806365",
+    "url": "/static/media/user.7de5e5f7.svg"
+  }
+]);
