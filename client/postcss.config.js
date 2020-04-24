e051011cba1ec0ef70e177d499564014e4278f33
tu pham <shammer280@gmail.com>
2020-04-24 10:22:14 +0700

    use postcss and autoprefixer to combine style

---
+++ client/postcss.config.js
@@ -0,0 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    cssnano({
+      preset: "default",
+    }),
+    purgecss({
+      content: ["./*.html", "./src/**/*.jsx", "./src/**/*.js"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import "./style/bootstrap.min.css";
+import "./style/main.css";
 import "./style/style.css";
-import "./style/animate.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-image-gallery/styles/css/image-gallery.css";
